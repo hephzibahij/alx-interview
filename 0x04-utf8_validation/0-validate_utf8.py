5155bc95646564b3bd88b76af4bbfd9c401c8cf5
Hephzibah <hephzibahijeje89@gmail.com>
2024-04-04 16:08:00 -0400

    files

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,38 +3,32 @@
 
 
 def validUTF8(data):
-    """
-    Method that determines if a given data set represents a valid
-    UTF-8 encoding.
-    """
-    number_bytes = 0
+    # Number of bytes in the current UTF-8 character
+    num_bytes = 0
 
-    mask_1 = 1 << 7
-    mask_2 = 1 << 6
+    for byte in data:
+        # If this is the start of a new UTF-8 character
+        if num_bytes == 0:
+            # Count the number of leading 1s in the byte
+            mask = 1 << 7
+            while mask & byte:
+                num_bytes += 1
+                mask >>= 1
 
-    for i in data:
-
-        mask_byte = 1 << 7
-
-        if number_bytes == 0:
-
-            while mask_byte & i:
-                number_bytes += 1
-                mask_byte = mask_byte >> 1
-
-            if number_bytes == 0:
+            # Invalid UTF-8 start byte
+            if num_bytes == 0:
                 continue
 
-            if number_bytes == 1 or number_bytes > 4:
+            # Single byte characters are valid
+            if num_bytes == 1 or num_bytes > 4:
+                return False
+        else:
+            # Check if the byte is a continuation byte
+            if (byte >> 6) != 0b10:
                 return False
 
-        else:
-            if not (i & mask_1 and not (i & mask_2)):
-                    return False
+        # Decrement the number of bytes remaining in the current character
+        num_bytes -= 1
 
-        number_bytes -= 1
-
-    if number_bytes == 0:
-        return True
-
-    return False
+    # All bytes have been processed and no incomplete characters are left
+    return num_bytes == 0
