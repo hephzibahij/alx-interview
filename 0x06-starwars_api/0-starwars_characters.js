0b8ea224c38e4003d14aeebe14d5276b3e94082d
Hephzibah <hephzibahijeje89@gmail.com>
2024-04-18 16:31:31 -0400

    files

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,40 +1,23 @@
 #!/usr/bin/node
-import requests
-import sys
 
+const request = require('request');
 
-def get_characters(movie_id):
-    url = f"https://swapi.dev/api/films/{movie_id}/"
-    response = requests.get(url)
-    if response.status_code == 200:
-        film_data = response.json()
-        characters = film_data['characters']
-        return characters
-    else:
-        print(f"Failed to fetch characters for movie with ID {movie_id}")
-        return []
+const movieId = process.argv[2];
 
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
 
-def get_character_names(characters):
-    character_names = []
-    for character_url in characters:
-        response = requests.get(character_url)
-        if response.status_code == 200:
-            character_data = response.json()
-            character_names.append(character_data['name'])
-        else:
-            character_names.append("Unknown Character")
-    return character_names
+request(url, async (err, res, body) => {
+  err && console.log(err);
 
+  const charactersArray = (JSON.parse(res.body).characters);
+  for (const character of charactersArray) {
+    await new Promise((resolve, reject) => {
+      request(character, (err, res, body) => {
+        err && console.log(err);
 
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: python script.py <movie_id>")
-        sys.exit(1)
-
-    movie_id = sys.argv[1]
-    characters = get_characters(movie_id)
-    character_names = get_character_names(characters)
-
-    for name in character_names:
-        print(name)
+        console.log(JSON.parse(body).name);
+        resolve();
+      });
+    });
+  }
+});
