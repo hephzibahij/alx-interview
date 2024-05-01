55900c7387044058a8cdfaa7445802ac5a876147
Hephzibah Owoma <128981877+hephzibahij@users.noreply.github.com>
2024-05-01 20:54:36 +0100

    Update 0-main.py

--- 0x08-making_change/0-main.py
+++ 0x08-making_change/0-main.py
@@ -1,10 +1,24 @@
 #!/usr/bin/python3
-"""
-Main file for testing
-"""
 
-makeChange = __import__('0-making_change').makeChange
+""" Contains makeChange function"""
 
-print(makeChange([1, 2, 25], 37))
 
-print(makeChange([1256, 54, 48, 16, 102], 1453))
+def makeChange(coins, total):
+    """
+    Returns: fewest number of coins needed to meet total
+        If total is 0 or less, return 0
+        If total cannot be met by any number of coins you have, return -1
+    """
+    if not coins or coins is None:
+        return -1
+    if total <= 0:
+        return 0
+    change = 0
+    coins = sorted(coins)[::-1]
+    for coin in coins:
+        while coin <= total:
+            total -= coin
+            change += 1
+        if (total == 0):
+            return change
+    return -1
